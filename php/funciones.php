f97d6a704477956482384b37561b77b83d2427a1
VaSanPue <vasanpue@gmail.com>
2023-11-23 20:23:04 +0100

    Creadas todas las funciones

--- php/funciones.php
+++ php/funciones.php
@@ -52,6 +52,110 @@
         } catch (Exception $e) {
             die("Conexión fallida: " . $e->getMessage());
         }
+    }
+
+    function getSuperficiePaises($continente, $numero) {
+        $paises = [];
+
+        try {
+            $conn = new mysqli(HOST, USER, PASS, DB);
+                
+            // Se obtiene la lista de todos los paises almacenados de un continente
+            $query = "SELECT Name, SurfaceArea FROM `country` WHERE Continent = '$continente' ORDER BY SurfaceArea LIMIT $numero";
+
+            $result = $conn->query($query);
+
+            if ($result && $result->num_rows > 0) {
+                $paises = $result->fetch_all(MYSQLI_ASSOC);
+            }
+
+            $conn->close();
+            return $paises;
+        } catch (Exception $e) {
+            die("Conexión fallida: " . $e->getMessage());
+        }
+    }
+
+    function getPoblaciónPaises($continente, $numero) {
+        $paises = [];
+
+        try {
+            $conn = new mysqli(HOST, USER, PASS, DB);
+                
+            // Se obtiene la lista de todos los paises almacenados de un continente
+            $query = "SELECT Name, Population FROM `country` WHERE Continent = '$continente' ORDER BY Population LIMIT $numero";
+
+            $result = $conn->query($query);
+
+            if ($result && $result->num_rows > 0) {
+                $paises = $result->fetch_all(MYSQLI_ASSOC);
+            }
+
+            $conn->close();
+            return $paises;
+        } catch (Exception $e) {
+            die("Conexión fallida: " . $e->getMessage());
+        }
+    }
+
+    function getPoblaciónCiudades($pais, $numero) {
+        $ciudades = [];
+
+        try {
+            $conn = new mysqli(HOST, USER, PASS, DB);
+                
+            // Se obtiene el codigo del país a buscar
+            $query = "SELECT Code FROM `country` WHERE Name = '$pais'";
+
+            $result = $conn->query($query);
+
+            if ($result && $result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+
+                // Almacenamos el codigo del pais en una variable
+                $codigoPais = $row['Code'];
+
+                // Con la variable del código del país obtenemos las ciudades del propio país
+                $queryCiudades =  "SELECT Name, Population FROM `city` WHERE CountryCode = '$codigoPais' ORDER BY Population LIMIT $numero";
+
+                $resultCiudades = $conn->query($queryCiudades);
+
+                if ($resultCiudades && $resultCiudades->num_rows > 0) {
+                    $ciudades = $resultCiudades->fetch_all(MYSQLI_ASSOC);
+                }
+            }
+
+            $conn->close();
+            return $ciudades;
+        } catch (Exception $e) {
+            die("Conexión fallida: " . $e->getMessage());
+        }
+    }
+
+    function mostrarTabla($datos) {
+        // Si el array no viene vacío lo recorremos
+        if(!empty($datos)) {
+            $columnas = array_keys($datos[0]);
+
+            echo '<table border="1">';
+            
+            // Creamos los encabezados
+            echo '<tr>';
+            foreach ($columnas as $columna) {
+                echo '<th>' . $columna . '</th>';
+            }
+            echo '</tr>';
         
+            // Creamos las filas de la tabla
+            foreach ($datos as $fila) {
+                echo '<tr>';
+                foreach ($fila as $valor) {
+                    echo '<td>' . $valor . '</td>';
+                }
+                echo '</tr>';
+            }
+        
+            echo '</table>';
+        }
     }
 ?>
